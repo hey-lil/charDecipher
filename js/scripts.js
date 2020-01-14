88814d552ba952f3848c278d1c7fc07fc06bccf1
Keturah Howard and Lillian Taft <keturah@>
2020-01-13 16:25:04 -0800

    make a new function that takes thee first and last letter of prompt answer, capitalize them, and reverse their order

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ var lastLetter = function(arg) {
 }
 console.log("lastLetter:", lastLetter(soup));
 
+// var newCapString = function(char1, char2) {
+//   var newString = char1 + char2;
+//   return newString.toUpperCase();
+// }
+// console.log(newCapString(firstLetter, lastLetter(soup)));
+
+var newStr = function(a,b) {
+  return b.toUpperCase() + a.toUpperCase();
+}
+console.log("newStr:",newStr(firstLetter, lastLetter(soup)));
+
+
